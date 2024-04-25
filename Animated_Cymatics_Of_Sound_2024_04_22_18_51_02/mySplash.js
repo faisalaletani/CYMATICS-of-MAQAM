2d47aff3574e45767b364cf693a7f76aaa62677a
faisalaletani <167809060+faisalaletani@users.noreply.github.com>
2024-04-25 19:59:28 -0400

    Add files via upload

--- Animated_Cymatics_Of_Sound_2024_04_22_18_51_02/mySplash.js
+++ Animated_Cymatics_Of_Sound_2024_04_22_18_51_02/mySplash.js
@@ -2,10 +2,9 @@ class Splash {
   constructor() {
     this.splashBorder = 100;
 
-    // Creating a smaller white rectangular background
     this.backgroundRect = createDiv('');
     this.backgroundRect.position(this.splashBorder+500, this.splashBorder);
-    this.backgroundRect.size(550, 400); // Adjust width and height as needed
+    this.backgroundRect.size(550, 400); 
     this.backgroundRect.style('background-color', '#8BC34A');
 
     fill(0, 0, 222);
@@ -22,7 +21,7 @@ class Splash {
     this.name = createDiv("FAISAL ALETANI");
     this.name.position(this.splashBorder + 700, this.splashBorder + 60);
 
-    this.info = createDiv("This project is dedicated to present the Arabic school of ART and GEOMETRY using an interactive geometry designer  which be used to imagine the cymatic representation of the Arabic music modes which is refered to as maqamat, once you click on the this splash page a 90 second music Taqsim,,, file will be played in the background which will consist use the the sliders below the canvas to adjust the geomytry shape based on the maqam you choose to explore.<p> <a href=https://editor.p5js.org/faisalfreezy/sketches/QckP5cAWp>view code</a>");
+    this.info = createDiv("This project is dedicated to present the Arabic school of ART and GEOMETRY using an interactive geometry designer which can be used to visualize the cymatic representation of the Arabic music modes or scale which is refered to as maqamat, once you click on the this splash page you will start a musical journey where you will here 8 different maqams or arabic scales in 90 seconds in the background as you try to create an arabic geometrical shape that would reflect a cymatic representation based on what you heared and felt.<p> <a href=https://editor.p5js.org/faisalfreezy/sketches/QckP5cAWp>view code</a>");
     this.info.position(this.splashBorder + 550, this.splashBorder + 100);
     this.info.size(500, 100); // Adjust width and height as needed
 
