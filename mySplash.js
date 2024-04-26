a728b4c80e001a05f47f59fa44a28b7eeba411bd
faisalaletani <167809060+faisalaletani@users.noreply.github.com>
2024-04-25 20:09:25 -0400

    Update mySplash.js

--- mySplash.js
+++ mySplash.js
@@ -21,7 +21,7 @@ class Splash {
     this.name = createDiv("FAISAL ALETANI");
     this.name.position(this.splashBorder + 700, this.splashBorder + 60);
 
-    this.info = createDiv("This project is dedicated to present the Arabic school of ART and GEOMETRY using an interactive geometry designer which can be used to visualize the cymatic representation of the Arabic music modes or scale which is refered to as maqamat, once you click on the this splash page you will start a musical journey where you will here 8 different maqams or arabic scales in 90 seconds in the background as you try to create an arabic geometrical shape that would reflect a cymatic representation based on what you heared and felt.<p> <a href=https://editor.p5js.org/faisalfreezy/sketches/QckP5cAWp>view code</a>");
+    this.info = createDiv("This project is dedicated to present the Arabic school of Art and Geometry using an interactive geometry designer which can be used to visualize the cymatic representation of the Arabic music modes or scale which is refered to as maqamat, once you click on the this splash page you will start a musical journey where you will here 8 different maqams or arabic scales in 90 seconds in the background as you try to create an arabic geometrical shape that would reflect a cymatic representation based on what you heared and felt.<p> <a href=https://editor.p5js.org/faisalfreezy/sketches/QckP5cAWp>view code</a>");
     this.info.position(this.splashBorder + 550, this.splashBorder + 100);
     this.info.size(500, 100); // Adjust width and height as needed
 
