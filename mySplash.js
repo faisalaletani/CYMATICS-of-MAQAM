6911476beef04af992a03a9a16c436d8d671976c
faisalaletani <167809060+faisalaletani@users.noreply.github.com>
2024-04-27 18:35:17 -0400

    Update mySplash.js

--- mySplash.js
+++ mySplash.js
@@ -4,7 +4,7 @@ class Splash {
 
     this.backgroundRect = createDiv('');
     this.backgroundRect.position(this.splashBorder+500, this.splashBorder);
-    this.backgroundRect.size(550, 450); 
+    this.backgroundRect.size(550, 550); 
     this.backgroundRect.style('background-color', '#8BC34A');
 
     fill(0, 0, 222);
